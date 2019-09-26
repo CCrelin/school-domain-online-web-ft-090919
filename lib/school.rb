741d38c20e6cdfd652ddaa5e0abcffea0856dcdd
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-26 23:32:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,16 +2,22 @@ class School
   attr_accessor(:name, :roster)
   
   def initialize(name, roster={})
+    
     @name = name
     @roster = roster
+  
   end
   
   def add_student(name, grade)
+    
     if @roster[grade] != nil 
       @roster[grade] << name
-    
     else
       @roster[grade] = [name] 
+    
     end
   end
+  
+   def grade(grade)
+   end
 end
