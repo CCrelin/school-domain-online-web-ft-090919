81e62df25de959d830d5b3aede41cd8a82738eda
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-26 23:23:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,12 +8,10 @@ class School
   
   def add_student(name, grade)
     if @roster[grade] != nil 
-    @roster[grade] << name
+      @roster[grade] << name
     
-  else
-    @roster[grade] = [name] 
-    
-    
-  
+    else
+      @roster[grade] = [name] 
+    end
   end
 end
